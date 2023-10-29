935c650d35c85d609755570972cae2ec82036e50
John Davison <john.davison@deskpro.com>
2023-10-29 08:44:37 +0000

    Create new version, force node 18 in eas config

--- app.config.ts
+++ app.config.ts
@@ -21,7 +21,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name,
   slug: "iq-puzzler-mobile",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "dark",
